6986c6b81f4f6be524f864751d13369d56dd3730
Roman Ortega Mondaca <romtega88@gmail.com>
2024-03-12 23:17:08 -0600

    useState for search bar and initial css for navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,23 +1,56 @@
+import { useState } from "react";
 import "./navbar.css";
 
 function Navbar() {
+  const [navbarSearch, setNavbarSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!navbarSearch) return;
+    console.log("Funciona: ", navbarSearch);
+    setNavbarSearch("");
+  };
+
+  const handleInput = (e) => {
+    setNavbarSearch(e.target.value);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar__logo">Logo</div>
-      <ul className="navbar__categories">
+    <nav className="navbar flex bg-primary">
+      <ul className="navbar__categories flex text-white" role="list">
+        <li className="navbar__item">
+          <div className="navbar__logo">Logo</div>
+        </li>
         <li className="navbar__item">Mas Buscados</li>
         <li className="navbar__item">Categorias</li>
         <li className="navbar__item">Sobre nosotros</li>
         <li className="navbar__item">Contacto</li>
       </ul>
-      <ul className="main-navbar__user">
-        <li className="navbar__search">
-          <input type="text" placeholder="Busca un producto" />
+      <ul className="navbar__user flex" role="list">
+        <li className="navbar__search bg-white">
+          <form className="navbar__form" onSubmit={handleSubmit}>
+            <input
+              className="navbar__input"
+              type="text"
+              placeholder="Busca un producto"
+              value={navbarSearch}
+              onChange={handleInput}
+            />
+            <button type="submit" className="navbar__btn">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </form>
         </li>
-        <li className="navbar__login">Icono de usuario</li>
-        <li className="navbar__fav">Icono de corazon</li>
-        <li className="navbar__cart">Icono de carrito</li>
-        <li className="navbar__total">Total</li>
+        <li className="navbar__icon bg-accent">
+          <i className="fa-regular fa-user fa-lg"></i>
+        </li>
+        <li className="navbar__icon bg-accent">
+          <i className="fa-regular fa-heart fa-lg"></i>
+        </li>
+        <li className="navbar__icon bg-accent">
+          <i className="fa-solid fa-cart-shopping fa-lg"></i>
+        </li>
+        <li className="navbar__item">Total</li>
       </ul>
     </nav>
   );
